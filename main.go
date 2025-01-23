3f0498c7f72beae9ddd616d19d0f1d7f43b6824f
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-22 23:34:00 -0500

    ok

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,9 +19,14 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "wails-demo-angualr",
-		Width:  1024,
-		Height: 768,
+		Title:             "wails-demo-angular",
+		Width:             1024,
+		Height:            768,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         false,
+		StartHidden:       false,
+		HideWindowOnClose: false,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
@@ -28,6 +35,12 @@ func main() {
 		Bind: []interface{}{
 			app,
 		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:    false,
+		},
+		LogLevel: logger.DEBUG,
 	})
 	if err != nil {
 		println("Error:", err.Error())
